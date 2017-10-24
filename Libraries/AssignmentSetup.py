50c037aad6a0bca2b74cd9aef7b0e03f476bee57
LoicPelissier <33062999+LoicPelissier@users.noreply.github.com>
2017-10-24 23:01:45 +0200

    Update AssignmentSetup.py

--- Libraries/AssignmentSetup.py
+++ Libraries/AssignmentSetup.py
@@ -3,9 +3,9 @@ import shutil, json, time, os
 def RestoreJsonDb():
 	print '*INFO* Restoring Json Db'
 	print '*INFO* Working dir :'+os.getcwd()
-	shutil.copy('./Backup/db.json','../assignment-qa-master/src/server/')
+	shutil.copy('./Backup/db.json','../assignment-qa/src/server/')
 	time.sleep(0.5)
-	fd=open('../assignment-qa-master/src/server/db.json','r')
+	fd=open('../assignment-qa/src/server/db.json','r')
 	print '*INFO* DB Content: ' + fd.read()
 	fd.close()
 
@@ -16,7 +16,7 @@ def CreateJsonDb(RecordCount):
 	for Idx in range(1,int(RecordCount)+1):
 		print '*INFO* Creating line '+str(Idx)
 		Data["pets"].append( { "id" : Idx, "name" : "NAME"+str(Idx).zfill(3), "status" : "STATUS"+str(Idx).zfill(3) } )
-	fd=open("../assignment-qa-master/src/server/db.json","w")
+	fd=open("../assignment-qa/src/server/db.json","w")
 	fd.write(json.dumps(Data))
 	fd.close()
 	time.sleep(0.5)
